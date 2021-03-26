8a0af0a463494f4933223a9ff00b168db6cbbd84
dericsp <dericsp@gmail.com>
2021-03-26 08:25:32 -0300

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 const imagemin = require('gulp-imagemin');
 const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
 
+
+gulp.task('minify-css', () => {
+    return gulp.src('components/*.css')
+      .pipe(cleanCSS({debug: true}, (details) => {
+        console.log(`${details.name}: ${details.stats.originalSize}`);
+        console.log(`${details.name}: ${details.stats.minifiedSize}`);
+      }))
+    .pipe(gulp.dest('components'));
+  });
+  
 function defaultTask(cb) {
-    gulp.src('public/*')
-  .pipe(imagemin())
-  .pipe(gulp.dest('public/'))
+//     gulp.src('public/*')
+//   .pipe(imagemin())
+//   .pipe(gulp.dest('public/'))
     
     cb();
 }
