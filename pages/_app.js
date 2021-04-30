29dffaca6d328b1559f3f582fc424b1e7c1a3ced
dericsp <dericsp@gmail.com>
2021-04-30 18:44:46 -0300

    alt whatsapp logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,35 @@
 import '../components/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
 import ReactGA from 'react-ga';
 import GoogleTagManager from '../components/GoogleTagManager'
 
-function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url, { shallow }) => {
-      ReactGA.set({ page: url })
-      ReactGA.pageview(url)
-    }
-    ReactGA.initialize('UA-66520244-1', { debug: false })
-    ReactGA.set({ page: router.pathname })
-    ReactGA.pageview(router.pathname)
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  },
+function MyApp ({ Component, pageProps }) {
+    const router = useRouter()
+    useEffect(() => {
+      const handleRouteChange = (url, { shallow }) => {
+        ReactGA.set({ page: url })
+        ReactGA.pageview(url)
+      }
+      ReactGA.initialize('UA-66520244-1', { debug: false })
+      ReactGA.set({ page: router.pathname })
+      ReactGA.pageview(router.pathname)
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    },
     [])
-
-  return (
-    <GoogleTagManager>
+  
+    return (
+        
+          <GoogleTagManager>
       <Component {...pageProps} />
     </GoogleTagManager>
-  )
-}
-
-export default MyApp
+    )
+  }
+  
+  export default MyApp
 
 
